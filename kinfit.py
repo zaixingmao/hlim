c7aaebb817c3e6b7123da379a397a7450ce76029
Edward Laird <elaird@gmail.com>
2014-09-03 13:39:26 +0200

    add samples; tweak cosmetics

--- kinfit.py
+++ kinfit.py
@@ -58,8 +58,8 @@ def loopMulti(fileNames=[], nEventsMax=None):
            "m_vs_m": [],
            }
 
-    for iFile, fileName in enumerate(fileNames):
-        chi2, m, mNo, mm = loop(fileName, nEventsMax, suffix="_%d" % iFile)
+    for fileName, leg in fileNames:
+        chi2, m, mNo, mm = loop(fileName, nEventsMax, suffix="_%s" % leg)
         out["chi2"].append(chi2)
         out["m_fit"].append(m)
         out["m_no_fit"].append(mNo)
@@ -69,7 +69,8 @@ def loopMulti(fileNames=[], nEventsMax=None):
 
 def loop(fileName="", nEventsMax=None, suffix=""):
     h_chi2 = r.TH1D("h_chi2%s" % suffix, ";chi2;events / bin", 120, -10.0, 390.0)
-    bins = [120, -10.0, 590.0]
+    #bins = [120, -10.0, 590.0]
+    bins = [60, -10.0, 590.0]
     h_m = r.TH1D("h_m%s" % suffix, ";m_{fit} (GeV);events / bin", *bins)
     h_fMass = r.TH1D("h_fMass%s" % suffix, ";m_{no fit} (GeV);events / bin", *bins)
 
@@ -113,29 +114,51 @@ def loop(fileName="", nEventsMax=None, suffix=""):
 
 def pdf(fileName="", histos={}):
     out = "check.pdf"
-    can = r.TCanvas()
+    can = r.TCanvas("can", "", 2)
     
     can.Print(out+"[")
+
+    leg = r.TLegend(0.65, 0.65, 0.85, 0.85)
+    leg.SetFillStyle(0)
+    leg.SetBorderSize(0)
+    fillLeg = True
+
     for key, lst in sorted(histos.iteritems()):
+        color = 1
         for i, h in enumerate(lst):
-            if h.ClassName().startswith("TH1"):
+            th1 = h.ClassName().startswith("TH1")
+            if th1:
                 integral = h.Integral(0, 1 + h.GetNbinsX())
                 if integral:
                     h.Scale(1.0 / integral)
                     h.GetYaxis().SetTitle("fraction of events / bin")
+                    h.GetYaxis().SetTitleOffset(1.3)
                 else:
                     continue
 
+            if fillLeg:
+                name = h.GetName().split("_")[-1]
+                #text = "%s (%d)" % (name, integral)
+                text = name
+                leg.AddEntry(h, text, "le")
             h.SetStats(False)
-            h.SetLineColor(1 + i)
-            h.SetMarkerColor(1 + i)
+            h.SetLineColor(color)
+            h.SetMarkerColor(color)
+
+            color += 1
+            if color == 5:
+                color += 1
+
+            gopts = "ehist" if th1 else ""
             if i:
-                h.Draw("same")
+                h.Draw("%ssame" % gopts)
             else:
-                h.Draw()
+                h.Draw(gopts)
 
         r.gPad.SetTickx()
         r.gPad.SetTicky()
+        leg.Draw()
+        fillLeg = False
         can.Print(out)
     can.Print(out+"]")
 
@@ -144,6 +167,7 @@ if __name__ == "__main__":
     r.gROOT.SetBatch(True)
     r.gErrorIgnoreLevel = 2000
     #r.gStyle.SetOptStat("e")
+    r.TH1.SetDefaultSumw2(True)
 
     hypo_mh1 = r.std.vector('Int_t')()
     hypo_mh2 = r.std.vector('Int_t')()
@@ -154,9 +178,13 @@ if __name__ == "__main__":
     setup(path="/afs/cern.ch/user/%s/%s/HHKinFit" % (user[0], user),
           lib="libHHKinFit.so",
           )
-    histos = loopMulti(fileNames=["v2/H2hh260_all.root",
-                                  "v2/H2hh300_all.root",
-                                  "v2/H2hh350_all.root"],
+    histos = loopMulti(fileNames=[("v2/H2hh260_all.root",     "H260"),
+                                  ("v2/H2hh300_all.root",     "H300"),
+                                  ("v2/H2hh350_all.root",     "H350"),
+                                  ("v2/tt_eff_all.root",      "tt-dilep"),
+                                  ("v2/tt_semi_eff_all.root", "tt-semilep"),
+                                  #("v2/ZZ_eff_all.root",      "ZZ"),
+                                  ],
                        #nEventsMax=200,
                        )
     pdf(fileName="check.pdf", histos=histos)
