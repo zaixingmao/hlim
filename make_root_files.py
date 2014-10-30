024a0288af36c5303fdec916161ea57d072935c1
Edward Laird <elaird@gmail.com>
2014-10-30 11:02:52 +0100

    allow multiple cuts; collect specs

--- make_root_files.py
+++ make_root_files.py
@@ -31,7 +31,7 @@ def shift(h):
     combineBinContentAndError(h, 1, 0)  # underflows
 
 
-def histos(fileName="", bins=None, procs=[], var="", rescaleX=False, cut=None):
+def histos(fileName="", bins=None, procs=[], var="", rescaleX=False, cuts={}):
     assert fileName
     assert bins
 
@@ -53,9 +53,7 @@ def histos(fileName="", bins=None, procs=[], var="", rescaleX=False, cut=None):
         h.Sumw2()
         w = "1.0" if proc.startswith("data") else "triggerEff"
         cutString = 'sampleName=="%s"' % proc
-        if cut:
-            assert len(cut) == 3, cut
-            cutVar, cutMin, cutMax = cut
+        for cutVar, (cutMin, cutMax) in sorted(cuts.iteritems()):
             if cutMin is not None:
                 cutString += " && (%g < %s)" % (cutMin, cutVar)
             if cutMax is not None:
@@ -118,8 +116,19 @@ def describe(h):
         print s
     print
 
+def cutDesc(cuts):
+    descs = []
+    for cutVar, (cutMin, cutMax) in sorted(cuts.iteritems()):
+        cutDesc = cutVar
+        if cutMin is not None:
+            cutDesc = "%.1f.%s" % (cutMin, cutDesc)
+        if cutMax is not None:
+            cutDesc = "%s.%.1f" % (cutDesc, cutMax)
+        descs.append(cutDesc)
+    return "_".join(descs)
+
 def go(inFile="", sFactor=None, sKey="", bins=None, var="", rescaleX=True,
-       cut=None, lumi=19.0):
+       cuts=None, lumi=19.0):
 
     assert type(sFactor) is int, type(sFactor)
     assert bins
@@ -134,8 +143,8 @@ def go(inFile="", sFactor=None, sKey="", bins=None, var="", rescaleX=True,
              "dataOSRelax": "QCD",
              }
 
-    hs = histos(fileName=inFile, procs=procs.keys(), bins=bins, var=var,
-                rescaleX=rescaleX, cut=cut)
+    hs = histos(fileName=inFile, procs=procs.keys(), bins=bins,
+                var=var, rescaleX=rescaleX, cuts=cuts)
 
     hs["tt_full"].Add(hs["tt_semi"])
     del hs["tt_semi"]
@@ -143,13 +152,9 @@ def go(inFile="", sFactor=None, sKey="", bins=None, var="", rescaleX=True,
     dir = "root"
     mkdir(dir)
     fileName = "%s/%dx%s_%s" % (dir, sFactor, sKey.replace("H2hh", ""), var)
-    if cut:
-        cutDesc = cut[0]
-        if cut[1] is not None:
-            cutDesc = "%d.%s" % (cut[1], cutDesc)
-        if cut[2] is not None:
-            cutDesc = "%s.%d" % (cutDesc, cut[2])
-        fileName += "_%s" % cutDesc
+
+    if cutDesc(cuts):
+        fileName += "_%s" % cutDesc(cuts)
 
     f = r.TFile("%s.root" % fileName, "RECREATE")
     f.mkdir("tauTau_2jet2tag").cd()
@@ -207,17 +212,104 @@ def loop(inFile="", specs={}):
                    **spec)
 
 
+def specs1():
+    return [#{"var": "BDT_260", "bins": ( 4, -0.55, 0.25), "cuts": {}},
+            #{"var": "BDT_300", "bins": ( 4, -0.50, 0.30), "cuts": {}},
+            #{"var": "BDT_350", "bins": ( 4, -0.45, 0.35), "cuts": {}},
+            #{"var": "mJJReg",  "bins": (15, 50.0, 200.0), "cuts": {}},
+            {"var": "mJJ",     "bins": (15, 50.0, 200.0), "cuts": {}},
+            
+            {"var": "svMass",  "bins": (15, 50.0, 200.0), "cuts": {}},
+            {"var": "svMass",  "bins": (10, 50.0, 250.0), "cuts": {"mJJ": (90.0, 140.0)}},
+            
+            {"var": "fMass",   "bins": (7, 200.0, 480.0), "cuts": {}},
+            {"var": "fMassKinFit", "bins": (7, 200.0, 480.0), "cuts": {}},
+            
+            {"var": "fMass",   "bins": (7, 200.0, 480.0), "cuts": {"mJJ": (90.0, 140.0)}},  # , "svMass": (95.0, 155.0)}},
+            {"var": "fMassKinFit", "bins": (7, 200.0, 480.0), "cuts": {"mJJ": (90.0, 140.0)}},  # , "svMass": (95.0, 155.0)}},
+            
+            {"var": "chi2KinFit", "bins": (10, 0.0, 100.0), "cuts": {}},
+            {"var": "chi2KinFit", "bins": (10, 0.0, 100.0), "cuts": {}},
+            
+            #{"var": "fMass",   "bins": (5, 200.0, 400.0), "cuts": {"mJJ": (90.0, 140.0), "svMass": (95.0, 155.0)}},
+            #{"var": "fMassKinFit", "bins": (5, 200.0, 400.0), "cuts": {"mJJ": (90.0, 140.0), "svMass": (95.0, 155.0)}},
+            ]
+
+
+def specs2():
+    out = []
+    for var, bins in [("svMass",      (15,  50.0, 200.0)),
+                      ("fMass",       ( 7, 200.0, 480.0)),
+                     #("fMassKinFit", ( 7, 200.0, 480.0)),
+                     #("fMassKinFit", ( 6, 250.0, 490.0)),
+                      ("fMassKinFit", (12, 250.0, 490.0)),
+                     #("fMassKinFit", (24, 250.0, 490.0)),
+                      ("chi2KinFit",  (11, -10.0, 100.0)),
+                      ]:
+        for cut in [{},
+                    {"mJJ": (90.0, 140.0)},
+                    #{"chi2KinFit": (0.0, 40.0)},
+                    {"chi2KinFit": (0.0, 10.0)},
+                    ]:
+            out.append({"var": var, "bins": bins, "cuts": cut})
+    return out
+
+
+def specs3():
+    out = []
+    for var, bins in [#("svMass",      (12,  75.0, 195.0)),
+                      #("fMass",       ( 7, 200.0, 480.0)),
+                      #("fMass",       ( 6, 250.0, 490.0)),
+                      #("fMassKinFit", ( 6, 250.0, 490.0)),
+                      #("chi2KinFit",  (11, -10.0, 100.0)),
+                      ("BDT_260_2", (26, -1.0, 0.30)),
+                      ("BDT_300_2", (14, -1.0, 0.4)),
+                      ("BDT_350_2", (14, -1.0, 0.4)),
+                      ("BDT_260_3", (14, -1.0, 0.4)),
+                      ("BDT_300_3", (14, -1.0, 0.4)),
+                      ("BDT_350_3", (14, -1.0, 0.4)),
+                      ("BDT_260_18", (12, -1.0, 0.2)),
+                      ("BDT_300_18", (12, -1.0, 0.2)),
+                      ("BDT_350_18", (13, -1.0, 0.3)),
+                      ]:
+        for cut in [{},
+                    #{"mJJ": (90.0, 140.0)},
+                    #{"chi2KinFit": (0.0, 10.0)},
+                    ]:
+            out.append({"var": var, "bins": bins, "cuts": cut})
+    return out
+
+
+def specs4():
+    twoCuts = {"mJJ": (90.0, 140.0),
+               "svMass": (90.0, 140.0),
+               }
+
+    return [#{"var": "fMass",       "bins": ( 5, 250.0, 450.0), "cuts": {"BDT_300_3": (0.0, None)}, },
+            #{"var": "fMassKinFit", "bins": ( 5, 250.0, 450.0), "cuts": {"BDT_300_3": (0.0, None)}, },
+            #{"var": "fMass",       "bins": ( 4, 250.0, 410.0), "cuts": {"BDT_300_3": (0.2, None)}, },
+            #{"var": "fMassKinFit", "bins": ( 4, 250.0, 410.0), "cuts": {"BDT_300_3": (0.2, None)}, },
+            {"var": "fMass",       "bins": ( 4, 250.0, 410.0), "cuts": twoCuts},
+            {"var": "fMassKinFit", "bins": ( 4, 250.0, 410.0), "cuts": twoCuts},
+            #{"var": "chi2KinFit", "bins":  (4, -20.0, 60.0), "cuts": twoCuts},
+            ]
+
+
+def specs5():
+    out = []
+    for xMin in [80, 85, 90, 95, 100, 105, 110, 115, 120]:
+        for xMax in [130, 135, 140, 145, 150]:
+            twoCuts = {"mJJ": (xMin, xMax), "svMass": (xMin, xMax)}
+            out.append({"var": "fMassKinFit",
+                        "bins": ( 4, 250.0, 410.0),
+                        "cuts": twoCuts})
+    return out
+
+
 if __name__ == "__main__":
     r.gROOT.SetBatch(True)
     r.gErrorIgnoreLevel = 2000
 
-    loop(inFile="combined.root",
-         specs=[{"var": "BDT_260", "bins": ( 4, -0.55, 0.25), "cut": []},
-                {"var": "BDT_300", "bins": ( 4, -0.50, 0.30), "cut": []},
-                {"var": "BDT_350", "bins": ( 4, -0.45, 0.35), "cut": []},
-                {"var": "svMass",  "bins": (15, 50.0, 200.0), "cut": []},
-                {"var": "svMass",  "bins": (15, 50.0, 200.0), "cut": ("mJJ", 90.0, 140.0)},
-                #{"var": "mJJReg",  "bins": (15, 50.0, 200.0), "cut": []},
-                {"var": "mJJ",     "bins": (15, 50.0, 200.0), "cut": []},
-                ],
+    loop(inFile="combined_newTraining.root",
+         specs=specs5(),
          )
