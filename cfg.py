767f877c692fdacbf44bd48691c1100dc8c09d27
Edward Laird <elaird@gmail.com>
2014-11-04 14:11:33 +0100

    change names; add categories

--- cfg.py
+++ cfg.py
@@ -1,3 +1,5 @@
-#spin0 = [260, 300, 350]
-spin0 = range(260, 360, 10) + [500, 700]
-spin2 = [500, 700]
+#masses_spin0 = [260, 300, 350]
+masses_spin0 = range(260, 360, 10) + [500, 700]
+masses_spin2 = [500, 700]
+
+categories = [1, 2]
