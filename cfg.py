88de54d8f497968ab8537c80d3f0038a1d481e03
Edward Laird <elaird@gmail.com>
2014-11-12 15:35:52 +0100

    revert categories

--- cfg.py
+++ cfg.py
@@ -10,16 +10,16 @@ root_dest = "%s/src/auxiliaries/shapes/Brown" % os.environ["CMSSW_BASE"]
 masses_spin0 = range(260, 360, 10) #+ [500, 700]
 masses_spin2 = [500, 700]
 
-categories = {"MM_LM": "tauTau_2jet2tag",
-              #"2M": "tauTau_2jet2tag",
-              #"1M": "tauTau_2jet1tag",
+categories = {#"MM_LM": "tauTau_2jet2tag",
+              "2M": "tauTau_2jet2tag",
+              "1M": "tauTau_2jet1tag",
               }
 
 multi_vars = ["",
               #"fMassKinFit_0.0.chi2KinFit2.10.0",
               #"fMassKinFit_70.0.mJJ.150.0_90.0.svMass.150.0",
               "fMassKinFit_0.0.fMassKinFit_70.0.mJJ.150.0_90.0.svMass.150.0",
-              "BDT_260",
+              #"BDT_260",
               #"BDT_270",
               #"BDT_280",
               #"BDT_290",
