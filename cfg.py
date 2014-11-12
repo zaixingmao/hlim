0b4a3762f0fd8f1da59d99a93c9eaea20568e945
Edward Laird <elaird@gmail.com>
2014-11-12 10:18:38 +0100

    add vars

--- cfg.py
+++ cfg.py
@@ -3,3 +3,19 @@ masses_spin0 = range(260, 360, 10) #+ [500, 700]
 masses_spin2 = [500, 700]
 
 categories = [1, 2]
+
+multi_vars = ["",
+              "fMassKinFit_0.0.chi2KinFit2.10.0",
+              "fMassKinFit_70.0.mJJ.150.0_90.0.svMass.150.0",
+              "fMassKinFit_0.0.fMassKinFit_70.0.mJJ.150.0_90.0.svMass.150.0",
+              #"BDT_260",
+              #"BDT_270",
+              #"BDT_280",
+              #"BDT_290",
+              #"BDT_300",
+              #"BDT_310",
+              #"BDT_320",
+              #"BDT_330",
+              #"BDT_340",
+              #"BDT_350",
+              ]
