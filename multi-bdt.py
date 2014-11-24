fbaaec316f9746ad7b271179d604991740d698ba
Edward Laird <elaird@gmail.com>
2014-11-24 12:10:18 +0100

    make redirect optional

--- multi-bdt.py
+++ multi-bdt.py
@@ -7,6 +7,7 @@ import os
 cats = cfg.cats()
 workDir = cfg.workDir()
 inDir = "root"
+redirect = False
 
 for mass in cfg.masses_spin0:
     for fileName in os.listdir(inDir):
@@ -18,18 +19,24 @@ for mass in cfg.masses_spin0:
         os.system("rm -rf %s" % dirName)
         os.system("mkdir %s" % dirName)
         print "  (root)"
-        os.system("./make_root_files.py %s/%s > %s/yields.txt" % (inDir, fileName, dirName))
+        cmd = "./make_root_files.py %s/%s" % (inDir, fileName)
+
+        if redirect:
+            cmd += " > %s/yields.txt" % dirName
+        os.system(cmd)
 
         print "  (limit)"
-        os.system(" ".join(["cd %s &&" % workDir,
-                            "./go.py",
-                            "--file=%s" % cfg.outFileName(var="BDT", cuts={}),
-                            "--full",
-                            "--postfitonlyone",
-                            "--masses='%s'" % mass,
-                            "--categories='%s'" % cats,
-                            ">& %s/limit.out" % dirName, 
-                            ]))
+        args = ["cd %s &&" % workDir,
+                "./go.py",
+                "--file=%s" % cfg.outFileName(var="BDT", cuts={}),
+                "--full",
+                "--postfitonlyone",
+                "--masses='%s'" % mass,
+                "--categories='%s'" % cats,
+                ]
+        if redirect:
+            args.append("> %s/limit.out" % dirName)
+        os.system(" ".join(args))
 
         files = ["tt_ggHTohh-limit.pdf", "tt_ggHTohh-limit.txt"]
         for cat in cats.split():
