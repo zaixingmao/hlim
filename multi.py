6065d34c522ec12d1b2a19bb4583942b4509f14f
Edward Laird <elaird@gmail.com>
2015-01-15 17:41:35 +0100

    comment broken files

--- multi.py
+++ multi.py
@@ -20,7 +20,10 @@ for d in cfg.variables():
     dirName = "%s_%s" % (d["var"], cfg.cutDesc(d["cuts"]))
     os.system("rm -rf %s" % dirName)
     os.system("mkdir %s" % dirName)
-    it = "%s/src/auxiliaries/shapes/Italians/htt_tt.inputs-Hhh-8TeV_m_ttbb_kinfit_KinFitConvergedWithMassWindow.root" % os.environ["CMSSW_BASE"]
+    aux = "%s/src/auxiliaries/shapes" % os.environ["CMSSW_BASE"]
+
+    it = "%s/Italians/htt_tt.inputs-Hhh-8TeV_m_ttbb_kinfit_KinFitConvergedWithMassWindow.root" % aux
+    #br = "%s/Brown/fMassKinFit_0.0.fMassKinFit_70.0.mJJ.150.0_90.0.svMass.150.0_new.root" % aux
     br = cfg.outFileName(var=d["var"], cuts=d["cuts"])
 
     cmd = " ".join(["cd %s &&" % workDir,
@@ -37,8 +40,8 @@ for d in cfg.variables():
     os.system(cmd)
 
     files = ["tt_ggHTohh-limit.pdf", "tt_ggHTohh-limit.txt"]
-    for cat in cats.split():
-        files.append("../test/tauTau_2jet%stag_prefit_8TeV_LIN.pdf" % cat)
+    # for cat in cats.split():
+    #     files.append("../test/tauTau_2jet%stag_prefit_8TeV_LIN.pdf" % cat)
 
     for f in files:
         os.system("cp -p %s/%s %s/" % (workDir, f, dirName))
