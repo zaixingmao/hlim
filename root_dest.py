30b45006c8aae05c7cfc3b128a02fea346d44486
Edward Laird <elaird@gmail.com>
2015-01-16 14:36:54 +0100

    add check

--- root_dest.py
+++ root_dest.py
@@ -1,3 +1,8 @@
 import os
+import sys
+
+
+if "CMSSW_BASE" not in os.environ:
+    sys.exit("Set up the CMSSW environment.")
 
 root_dest = "%s/src/auxiliaries/shapes/Brown" % os.environ["CMSSW_BASE"]
