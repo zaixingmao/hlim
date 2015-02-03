360b8c12247b0418aea7b1c790ca81adaee08692
Edward Laird <elaird@gmail.com>
2015-02-03 14:59:59 +0100

    implement variable width (in progress)

--- determine_binning.py
+++ determine_binning.py
@@ -53,3 +53,47 @@ def fixed_width(width=0.1, threshold_r=0.2, threshold_l=0.1):
     x1_r = h.GetBinLowEdge(1 + iBin)
 
     return binning(h, width, xn_l, x1_r)
+
+
+def variable_width(minWidth=0.1, threshold=0.2, debug=True):
+    h = fine_histo()
+    low_edges = []
+
+    iBin = 1 + h.GetNbinsX()
+    if debug:
+        header = "   ".join(["iBin", "%6s" % "x", "delta", "   note", "%6s" % "x2", "iBin2", "low_edges"])
+        print header
+        print "-" * len(header)
+
+    while True:
+        iBin = bin_search(h, rargs=(iBin, -1, -1), threshold=threshold)
+        if iBin is None:
+            break
+
+        x = h.GetBinLowEdge(iBin)
+        fields = ["%4d" % iBin, "%6.3f" % x]
+
+        if low_edges:
+            fields.append("%5.3f" % (low_edges[-1] - x))
+            if minWidth <= (low_edges[-1] - x):
+                fields.append("%7s" % "ok")
+                low_edges.append(x)
+            else:
+                fields.append("%7s" % "revised")
+                x = low_edges[-1] - minWidth
+                low_edges.append(x)
+                iBin = h.FindBin(x)
+        else:
+            fields += ["fake", " " * 8]
+            low_edges.append(x + minWidth)
+            low_edges.append(x)
+
+        fields.append("%6.3f" % x)
+        fields.append(" %4d" % iBin)
+        if debug:
+            fields.append(str([float("%6.3f" % y) for y in low_edges]))
+            print "   ".join(fields)
+        iBin -= 1
+
+    low_edges.reverse()
+    return low_edges
