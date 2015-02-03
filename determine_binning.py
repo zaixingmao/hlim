2cf6a08d7b4f8b0750ec7ff405f8e09abdea8da9
Edward Laird <elaird@gmail.com>
2015-02-03 15:13:25 +0100

    enforce min-width via bin_search

--- determine_binning.py
+++ determine_binning.py
@@ -13,7 +13,7 @@ def histo(fileName, subdir="", name=""):
     return h
 
 
-def bin_search(h, rargs=(), threshold=None):
+def bin_search(h, rargs=(), threshold=None, iBinMax=None):
     assert threshold
 
     s = 0.0
@@ -24,7 +24,10 @@ def bin_search(h, rargs=(), threshold=None):
         e2 += e * e
         if 0 < s and math.sqrt(e2)/s < threshold:
             # print iBinX, h.GetBinLowEdge(iBinX), s, math.sqrt(e2), math.sqrt(e2)/s
-            return iBinX
+            if iBinMax is not None and iBinMax < iBinX:
+                continue
+            else:
+                return iBinX
     return None
 
 
@@ -55,42 +58,38 @@ def fixed_width(width=0.1, threshold_r=0.2, threshold_l=0.1):
     return binning(h, width, xn_l, x1_r)
 
 
-def variable_width(minWidth=0.1, threshold=0.2, debug=True):
-    h = fine_histo()
-    low_edges = []
-
-    iBin = 1 + h.GetNbinsX()
+def variable_width(minWidth=0.1, threshold=0.2, debug=False):
     if debug:
-        header = "   ".join(["iBin", "%6s" % "x", "delta", "   note", "%6s" % "x2", "iBin2", "low_edges"])
+        header = "   ".join(["iBin", "%6s" % "x", "delta", "low_edges"])
         print header
         print "-" * len(header)
 
+    h = fine_histo()
+    iBin = 1 + h.GetNbinsX()
+    low_edges = []
+
     while True:
-        iBin = bin_search(h, rargs=(iBin, -1, -1), threshold=threshold)
+        iBinMax = None
+        if low_edges:
+            iBinMax = h.FindBin(low_edges[-1] - minWidth)
+        iBin = bin_search(h, rargs=(iBin, -1, -1), threshold=threshold, iBinMax=iBinMax)
+
         if iBin is None:
             break
 
         x = h.GetBinLowEdge(iBin)
-        fields = ["%4d" % iBin, "%6.3f" % x]
-
         if low_edges:
-            fields.append("%5.3f" % (low_edges[-1] - x))
-            if minWidth <= (low_edges[-1] - x):
-                fields.append("%7s" % "ok")
-                low_edges.append(x)
-            else:
-                fields.append("%7s" % "revised")
-                x = low_edges[-1] - minWidth
-                low_edges.append(x)
-                iBin = h.FindBin(x)
+            if debug:
+                fields = ["%5.3f" % (low_edges[-1] - x)]
+            low_edges.append(x)
         else:
-            fields += ["fake", " " * 8]
+            if debug:
+                fields = [" fake"]
             low_edges.append(x + minWidth)
             low_edges.append(x)
 
-        fields.append("%6.3f" % x)
-        fields.append(" %4d" % iBin)
         if debug:
+            fields = ["%4d" % iBin, "%6.3f" % x] + fields
             fields.append(str([float("%6.3f" % y) for y in low_edges]))
             print "   ".join(fields)
         iBin -= 1
