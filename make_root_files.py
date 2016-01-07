1daa4a6cd36debef5f3ff4f768cc1fdf26b80f16
Edward Laird <elaird@gmail.com>
2016-01-07 14:17:54 -0500

    add option to skip variations; produce and describe sum_b variations

--- make_root_files.py
+++ make_root_files.py
@@ -78,7 +78,7 @@ def rescaled_bins(bins, variable):
     return bins, variable
 
 
-def histos(bins=None, variable="", cuts={}, category=""):
+def histos(bins=None, variable="", cuts={}, category="", skipVariations=False):
     assert bins
 
     # rescale so that bin width is 1.0
@@ -89,6 +89,9 @@ def histos(bins=None, variable="", cuts={}, category=""):
 
     out = {}
     for variation, fileName in cfg.files(variable).iteritems():
+        if skipVariations and variation:
+            continue
+
         f = r.TFile(fileName)
         tree = f.Get("eventTree")
         checkSamples(tree, fileName, variable, category)
@@ -259,7 +262,7 @@ def applyFactor(h=None, tfile=None, hName="", unit=False):
 
 
 def describe(h, l, keys):
-    print l, h.GetXaxis().GetTitle(), "(sum of %s)" % str(keys)
+    print l, h.GetXaxis().GetTitle(), "(sum of %s)" % str(sorted(keys))
     headers = "bin       x         cont  +-   err    (   rel)"
     print l, headers
     print l, "-" * len(headers)
@@ -271,7 +274,7 @@ def describe(h, l, keys):
         if c:
             s += "  (%5.1f%s)" % (100.*e/c, "%")
         print l, s
-    print l, "sum".ljust(12) + " = %9.3f" % h.Integral(0, 2 + h.GetNbinsX())
+    print l, "sum".ljust(12) + " = %9.3f" % h.Integral(0, 1 + h.GetNbinsX())
     print
 
 
@@ -294,7 +297,7 @@ def printTag(tag, l):
     print l, a
 
 
-def go(var={}, sFactor=0, sKey="", categoryWhitelist=None):
+def go(var={}, sFactor=0, sKey="", categoryWhitelist=None, skipVariations=False):
     assert var
     printHeader(**var)
 
@@ -303,7 +306,7 @@ def go(var={}, sFactor=0, sKey="", categoryWhitelist=None):
     for category, tag in cfg.categories.iteritems():
         if categoryWhitelist and category not in categoryWhitelist:
             continue
-        hs = histos(category=category, bins=var["bins"], variable=var["var"], cuts=var["cuts"])
+        hs = histos(category=category, bins=var["bins"], variable=var["var"], cuts=var["cuts"], skipVariations=skipVariations)
         if options.integrals or options.xs or options.contents:
             printTag(tag, l)
         f.mkdir(tag).cd()
@@ -333,10 +336,11 @@ def oneTag(tag, hs, sKey, sFactor, l):
         #h.Print("all")
         if cfg.isSignal(proc) and cfg.substring_signal_example not in proc:
             pass
-        elif proc.endswith("Down") or proc.endswith("Up"):
+        elif cfg.isVariation(proc):
             pass
         else:
             integrals.append((tag, proc, h.Integral(0, 2 + h.GetNbinsX())))
+
         h.Write()
 
     if options.integrals:
@@ -346,13 +350,32 @@ def oneTag(tag, hs, sKey, sFactor, l):
         fakeDataset(hs, sKey, sFactor, l).Write()
 
     if options.sumb:
-        h, keys = sumb(hs)
-        h.Write()
-        if options.contents and options.unblind:
-            describe(h, l, keys)
+        suffixes = sorted(cfg.files("DUMMY").keys())
+        for suffix in suffixes:
+            h, keys = sumb(hs, suffix=suffix)
+            if not h:  # due to go(skipVariations=True)
+                print "skipping", suffix
+                continue
+            h.Write()
+            if options.contents:
+                if suffix:
+                    nOld = len(l) + len(h.GetXaxis().GetTitle())
+                    print "%s %s = %9.3f  %s %s" % (l,
+                                                    "sum".ljust(12),
+                                                    h.Integral(0, 1 + h.GetNbinsX()),
+                                                    "(sum of %s)" % str(sorted([x.replace(suffix, "") for x in keys])),
+                                                    suffix,
+                                                    )
+                elif options.unblind:  # fake-dataset not created nor printed
+                    describe(h, l, keys)
+
+        if options.contents and any(suffixes):
+            print
 
 
-def sumb(hs, name="sum_b"):
+def sumb(hs, name="sum_b", suffix=""):
+    name += suffix
+
     d = None
     keys = []
     for key, histo in hs.iteritems():
@@ -360,7 +383,9 @@ def sumb(hs, name="sum_b"):
             continue
         if cfg.isData(key):
             continue
-        if cfg.isVariation(key):
+        if (not suffix) and cfg.isVariation(key):
+            continue
+        if suffix and not key.endswith(suffix):
             continue
 
         if d is None:
