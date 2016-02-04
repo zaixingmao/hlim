b063727d9a5ab81579b17432f4de87c56d0594ce
Edward Laird <elaird@gmail.com>
2016-02-04 10:55:58 -0500

    update, translate, unscale

--- bsm2h.py
+++ bsm2h.py
@@ -3,11 +3,25 @@
 import os
 import sys
 import ROOT as r
+from h2bsm import xs_fb
 
 
 def merge(stems=None, inDir=None, outDir=None, hName=None, suffix=None):
     outFile = r.TFile("%s/src/auxiliaries/shapes/Brown/merged_%s.root" % (os.environ["CMSSW_BASE"], hName), "RECREATE")
     outFile.mkdir(outDir)
+
+    name_map = [("QCD_all", "QCD"),
+                ("ZPrime_", "ggH"),
+                ("ZprimeToTauTau_M_", "ggH"),
+                ("Data", "data_obs"),
+                ("Diboson", "VV"),
+                ("TTBar", "TT"),
+                ("WJets", "W"),
+                ("ZJets", "ZTT"),
+
+                ("Z.root", "ZTT.root"),
+                ("eleTau_", ""),
+                ]
 
     for stem in stems:
         inFileName = inDir + stem + suffix
@@ -16,10 +30,20 @@ def merge(stems=None, inDir=None, outDir=None, hName=None, suffix=None):
         if not h1:
             sys.exit("%s:%s not found" % (inFileName, hName))
 
-        proc = stem.replace("_all", "").replace("ZPrime_", "ggH")
+        proc = stem
+        for old, new in name_map:
+            proc = proc.replace(old, new)
+
         h = h1.Clone(proc)
         h.SetDirectory(0)
         inFile.Close()
+
+        if proc.startswith("ggH"):
+            mass = int(proc.replace("ggH", ""))
+            if xs_fb(mass):
+                h.Scale(1000. / xs_fb(mass))  # some xs_fb --> 1 pb
+            else:
+                print proc, mass, "xs not found"
 
         outFile.cd(outDir)
         h.Write()
@@ -42,7 +66,25 @@ def m2():
     merge(stems=stems, hName=hName, inDir=d, outDir="eleTau_inclusive", suffix="_%s.root" % hName)
 
 
+def ele():
+    stems = ["eleTau_ZPrime_%d" % i for i in [500, 1000, 1500, 2000, 2500, 3000]]
+    stems += ["eleTau_VV", "eleTau_QCD", "eleTau_TT", "eleTau_W", "eleTau_Z", "eleTau_data_obs"]
+    d = "Fitter/eleTau_8TeVbins_900/"
+    hName = "m_effective"
+    merge(stems=stems, hName=hName, inDir=d, outDir="eleTau_inclusive", suffix=".root")
+
+
+def mu():
+    stems = ["ZprimeToTauTau_M_%d" % i for i in [500, 1000, 1500, 2000, 2500, 3000]]
+    stems += ["Data", "Diboson", "QCD", "TTBar", "WJets", "ZJets"]
+    d = "Fitter/"
+    hName = "DiJetMass"
+    merge(stems=stems, hName=hName, inDir=d, outDir="eleTau_inclusive", suffix="_muTauSR_ForFitter.root")
+
+
 if __name__ == "__main__":
     # m1("eleTau")
     # m1("emu")
-    m2()
+    # m2()
+    mu()
+    # ele()
