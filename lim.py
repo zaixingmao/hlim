9ec3254da5eb4f935896cb133ee18d3914ac58e6
Edward Laird <elaird@gmail.com>
2016-02-06 16:56:14 -0500

    store ml info

--- lim.py
+++ lim.py
@@ -4,7 +4,7 @@ import os
 import ROOT as r
 
 
-def filenames(ch="", masses=[]):
+def filenames_lim(ch="", masses=[]):
     out = []
     for m in masses:
         # print ch, m
@@ -17,6 +17,24 @@ def filenames(ch="", masses=[]):
         # tree = f.Get("limit")
         # tree.Scan("mh:limit:quantileExpected", "quantileExpected > 0.0")
         # f.Close()
+    return out
+
+
+def filenames_ml(ch="", masses=[]):
+    out = []
+
+    outFile = "ml_results_%s.txt" % ch
+    if os.path.exists(outFile):
+        os.remove(outFile)
+
+    for m in masses:
+        #"--saveNLL --saveShapes --saveNormalizations"
+        os.system('echo "\n%d" >> %s' % (m, outFile))
+        cmd = "combine -M MaxLikelihoodFit -m %d -n .Zprime.%s LIMITS/%s/%d/htt_%s_0_13TeV.txt | grep -A 1 'Best fit r'" % (m, ch, ch, m, ch)
+        os.system("%s >> %s" % (cmd, outFile))
+        dest = "higgsCombine.Zprime.%s.ML.mH%d.root" % (ch, m)
+        os.system("mv mlfit.Zprime.%s.root %s" % (ch, dest))
+        out.append(dest)
     return out
 
 
@@ -51,7 +69,24 @@ def dump(ch, d):
         print "   ".join(row)
 
 
+def diff_nuisances(ch="", filenames=[]):
+    prog = "%s/src/HiggsAnalysis/CombinedLimit/test/diffNuisances.py" % os.environ["CMSSW_BASE"]
+
+    outFile = "nuisances_diffs_%s.txt" % ch
+    if os.path.exists(outFile):
+        os.remove(outFile)
+
+    for filename in filenames:
+        os.system('echo "\n%s" >> %s' % (filename, outFile))
+        os.system("python %s %s >> %s" % (prog, filename, outFile))
+
+
 if __name__ == "__main__":
-    for ch in ["et", "em", "mt", "tt"][:2]:
-        chain = chained(filenames(ch=ch, masses=range(500, 3500, 500)))
+    masses = range(500, 3500, 500)
+    chs = ["et", "em", "mt", "tt"][:2]
+    for ch in chs:
+        chain = chained(filenames_lim(ch=ch, masses=masses))
         dump(ch, limits(chain))
+
+    for ch in chs:
+        diff_nuisances(ch, filenames_ml(ch, masses))
