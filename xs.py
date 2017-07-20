bcb774c24549cc8e6bec448dbaccf6a1455b50d8
Edward Laird <elaird@gmail.com>
2017-07-20 09:24:03 -0400

    make function names more explicit

--- xs.py
+++ xs.py
@@ -1,4 +1,4 @@
-def xs_fb(m):
+def xs_fb_lo_2015(m):
     return { 500: 9330.0,
              750: 1236.0,
             1000:  468.0,
@@ -14,9 +14,17 @@ def xs_fb(m):
             }.get(m)
 
 
-def xs_pb(m):
-    s = xs_fb(m)
+def xs_pb_lo_2015(m):
+    s = xs_fb_lo_2015(m)
     if s:
         return s / 1000.
     else:
         return None
+
+
+def xs_pb_nlo_2015(m):
+    k_factor = 1.3
+    lo = xs_pb_lo_2015(m)
+    if lo is None:
+        return lo
+    return k_factor * lo
