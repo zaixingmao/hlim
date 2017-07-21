cd39bea5976c1feaa8def5a8abd72bba2a5e61dd
Edward Laird <elaird@gmail.com>
2017-07-21 04:30:05 -0400

    tweak names and values

--- xs.py
+++ xs.py
@@ -1,30 +1,45 @@
-def xs_fb_lo_2015(m):
-    return { 500: 9330.0,
+def _fb_lo_2016(m):
+    return { 500: 5751.0,
              750: 1236.0,
-            1000:  468.0,
+            1000:  386.5,
             1250:  149.0,
-            1500:   72.3,
+            1500:   64.8,
             1750:   31.0,
-            2000:   17.3,
-            2500:   5.54,
+            2000:   15.8,
+            2500:   4.68,
             3000:   1.59,
             3500:   0.597,
             4000:   0.245,
-            # 4500:   0.17,
             }.get(m)
 
 
-def xs_pb_lo_2015(m):
-    s = xs_fb_lo_2015(m)
+def _fb_lo_2015(m):
+    return { 500: 9330.0,
+            1000:  468.0,
+            1500:   72.3,
+            2000:   17.3,
+            2500:   5.54,
+            3000:   1.29,
+            3500:   0.597,
+            4000:   0.245,
+            }.get(m)
+
+
+def pb_lo(m, year=None):
+    assert year in [2015, 2016]
+    if year == 2015:
+        s = _fb_lo_2015(m)
+    if year == 2016:
+        s = _fb_lo_2016(m)
     if s:
         return s / 1000.
     else:
         return None
 
 
-def xs_pb_nlo_2015(m):
+def pb_nlo(m, year):
     k_factor = 1.3
-    lo = xs_pb_lo_2015(m)
+    lo = pb_lo(m, year)
     if lo is None:
         return lo
     return k_factor * lo
